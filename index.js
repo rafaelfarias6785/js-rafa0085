87202eb1276d297f397949fb1168aad637f87401
rafaelfarias6785 <rafaelfarias6785@gmail.com>
2021-04-03 02:44:36 -0300

    1 file modified

--- index.js
+++ index.js
@@ -3,4 +3,16 @@ import './style.css';
 
 // Write Javascript code!
 const appDiv = document.getElementById('app');
-appDiv.innerHTML = `<h1>JS Starter</h1>`;
+appDiv.innerHTML = `<h1>JS Starter
+<br>
+rafa
+<br>
+<br>
+<br>
+<br>
+rafa
+<br>
+
+
+
+</h1>`;
